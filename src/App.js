105b5943656de5495ec693177a9a8df585b4df65
Rebecca <rebeccaquey@gmail.com>
2024-02-26 18:43:13 -0800

    update all the api routes to include all the right calls; send edit/delete calls to EventView

--- src/App.js
+++ src/App.js
@@ -3,28 +3,18 @@ import './App.css';
 import axios from 'axios';
 import { ChakraProvider } from '@chakra-ui/react';
 import moment from 'moment';
-import { Button, ButtonGroup } from '@chakra-ui/react';
 import CalendarView from './CalendarView.jsx';
+import EventView from './EventView';
+import UsersDropdownMenu from './UsersDropdownMenu';
+import CreateEvent from './CreateEvent';
 
 function App() {
   const [userData, setUserData] = useState();
+  const [currentUser, setCurrentUser] = useState(1);
+  const [userInviteData, setUserInviteData] = useState();
   const [eventData, setEventData] = useState();
-  const [inviteData, setInviteData] = useState();
+  const [eventInviteData, setEventInviteData] = useState();
   const [isLoaded, setIsLoaded] = useState(false);
-  const [showModal, setShowModal] = useState(false);
-  const [eventToEdit, setEventToEdit] = useState();
-  const [modalAction, setModalAction] = useState('');
-
-  const handleChange = (data) => {
-    setIsLoaded(false);
-    axios.post('http://localhost:3003/', data)
-      .then(result => {
-        console.log('Successful Post request: ', result);
-      })
-      .catch(err => {
-        console.log('Post request error: ', err);
-      });
-  }
 
   const getUsers = () => {
     axios.get('http://localhost:3003/users')
@@ -48,17 +38,19 @@ function App() {
     });
   }
 
-  const getEvent = (eventId) => {
-    axios.get(`http://localhost:3003/events/${eventId}`)
-      .then(({data}) => {
-        setEventData(data);
-        setIsLoaded(true);
-      })
-      .catch(err => {
-        console.log('Get request error: ', err)
-    });
-  }
+  //get event details for individual event --> might not call this route much?
+  // const getEvent = (eventId) => {
+  //   axios.get(`http://localhost:3003/events/${eventId}`)
+  //     .then(({data}) => {
+  //       setEventData(data);
+  //       setIsLoaded(true);
+  //     })
+  //     .catch(err => {
+  //       console.log('Get request error: ', err)
+  //   });
+  // }
 
+  //deletes the event if the owner clicks delete button ***
   const handleDeleteEvent = (eventId) => {
     axios.delete(`/events/${eventId}`)
     .then(result => {
@@ -67,11 +59,13 @@ function App() {
     .catch(err => {
       console.log('Delete request error: ', err);
     })
-    .then(setIsLoaded(false))
+    .then(setIsLoaded(false));
+
+    handleDeleteAllEventInvites(eventId);
   }
 
   const handleEditEvent = (eventId, eventDetails) => {
-    //event details should be all of the event details.. make sure to send that
+    //event details should be all of the event details.. make sure to send that! *****
     axios.patch(`/events/${eventId}`, {eventDetails})
     .then(result => {
       console.log('Successful Patch request: ', result);
@@ -79,52 +73,69 @@ function App() {
     .catch(err => {
       console.log('Patch request error: ', err);
     })
-    .then(setIsLoaded(false))
+    .then(setIsLoaded(false));
+
+    //all invites should change to 'Awaiting'
+    handleEditAllInvites(eventId);
+    //this changes invite response for current user to be 'yes'
+    handleEditInvite(currentUser, eventId, 'Yes');
   }
 
-  const handleAddEvent = (eventId, name, desc, start, length, location) => {
+  const handleAddEvent = (name, desc, location, start, length, invitees) => {
     let data = [{
       event_name: name,
       event_description: desc,
       event_start: start,
       event_length: length,
       event_location: location,
+      event_owner: currentUser,
       creation_time: moment().format(),
       last_edited: moment().format()
     }]
-    console.log(data);
-    axios.post(`http://localhost:3003/events/${eventId}`, data)
+
+    console.log('addevent', data, invitees);
+    axios.post(`http://localhost:3003/events`, data)
     .then(result => {
       console.log('Successful Post request: ', result);
     })
     .catch(err => {
       console.log('Post request error: ', err);
     })
-    .then(setIsLoaded(false))
+    .then(setIsLoaded(false));
+
+    //invitees are currently a string -- need to change to an array... ****
+    //also need to figure out what the userId is and send that
+
+    // invitees.forEach(user => {
+    //   handleAddInvite(user.id, eventData.length + 1);
+    // })
   }
 
   const getUserInvites = (userId) => {
-    axios.get(`http://localhost:3003/invites/${userId}`)
+    axios.get(`http://localhost:3003/invites/events/users/${userId}`)
       .then(({data}) => {
-        setInviteData(data);
-        setIsLoaded(true);
+        setUserInviteData(data);
       })
       .catch(err => {
         console.log('Get request error: ', err)
     });
   }
 
+  //this isnt doing anything right now... what does this post get? ****
   const getEventInvites = (eventId) => {
-    axios.get(`http://localhost:3003/invites/${eventId}`)
+    console.log('event', eventId);
+    axios.get(`http://localhost:3003/invites/events/${eventId}`)
       .then(({data}) => {
-        setInviteData(data);
-        setIsLoaded(true);
+        console.log('event test', data);
+        setEventInviteData(data);
+        // setEventData(data);
       })
       .catch(err => {
         console.log('Get request error: ', err)
     });
   }
 
+  //if owner deletes the event -- we need to delete all event invites
   const handleDeleteAllEventInvites = (eventId) => {
     axios.delete(`/invites/${eventId}`)
     .then(result => {
@@ -133,20 +144,20 @@ function App() {
     .catch(err => {
       console.log('Delete request error: ', err);
     })
-    .then(setIsLoaded(false))
   }
 
-  const handleDeleteAllUserInvites = (userId) => {
-    axios.delete(`/invites/${userId}`)
-    .then(result => {
-      console.log('Successful Delete request: ', result);
-    })
-    .catch(err => {
-      console.log('Delete request error: ', err);
-    })
-    .then(setIsLoaded(false))
-  }
+  //deletes all invites for user (if deleting a user) --> wont be called right now
+  // const handleDeleteAllUserInvites = (userId) => {
+  //   axios.delete(`/invites/${userId}`)
+  //   .then(result => {
+  //     console.log('Successful Delete request: ', result);
+  //   })
+  //   .catch(err => {
+  //     console.log('Delete request error: ', err);
+  //   })
+  // }
 
+  //deletes invite for individual user -- owner can remove people from invitation list...
   const handleDeleteInvite = (userId, eventId) => {
     axios.delete(`/invites/${userId}/${eventId}`)
     .then(result => {
@@ -155,7 +166,6 @@ function App() {
     .catch(err => {
       console.log('Delete request error: ', err);
     })
-    .then(setIsLoaded(false))
   }
 
   const handleEditAllInvites = (eventId) => {
@@ -166,7 +176,6 @@ function App() {
     .catch(err => {
       console.log('Patch request error: ', err);
     })
-    .then(setIsLoaded(false))
   }
 
   const handleEditInvite = (userId, eventId, response) => {
@@ -179,7 +188,6 @@ function App() {
     .catch(err => {
       console.log('Patch request error: ', err);
     })
-    .then(setIsLoaded(false))
   }
 
   const handleAddInvite = (userId, eventId) => {
@@ -187,7 +195,7 @@ function App() {
       user_id: userId,
       event_id: eventId,
     }]
-    console.log(data);
+    console.log('addinvite', data);
     axios.post(`http://localhost:3003/invites`, data)
     .then(result => {
       console.log('Successful Post request: ', result);
@@ -195,43 +203,59 @@ function App() {
     .catch(err => {
       console.log('Post request error: ', err);
     })
-    .then(setIsLoaded(false))
   }
 
-  const handleHideModal = () => {
-    setShowModal(false);
-  }
-
-  const handleModalClick = (action) => {
-    setShowModal(!showModal);
-    setModalAction(action);
-  }
-
-  const handleEditClick = (e) => {
-    setEventToEdit(e)
+  const handleSwitchUser = (userId, e) => {
+    setCurrentUser(userId);
+    getUserInvites(userId);
   }
 
   useEffect(() => {
     getEvents();
+    getUsers();
+    getUserInvites(currentUser);
   }, [isLoaded]);
-
 
   return (
     <ChakraProvider>
       <div className="App">
         <header className="App-header">
           <span>
-            Rebecca
+            <div>
+              EVENTS
+            </div>
+            <CreateEvent
+              handleAddEvent={handleAddEvent}
+              userData={userData}
+              currentUser={currentUser}
+            />
             {' '}
-            <Button variant="outline" colorScheme="green">
-            {/* onClick={} --> have a dropdown where it lists all the users and you can pick to show the diff events that user has */}
-              R
-            </Button>
+            <UsersDropdownMenu
+              userData={userData}
+              currentUser={currentUser}
+              handleSwitchUser={handleSwitchUser}
+              getUserInvites={getUserInvites}
+              eventData={eventData}
+            />
           </span>
         </header>
-        <body className="App-body">
-          <CalendarView />
-        </body>
+        {/* default can be calendar view, but on click, change to event view */}
+        <CalendarView
+          eventData={eventData}
+          getEventInvites={getEventInvites}
+          userInviteData={userInviteData}
+        />
+        <EventView
+          eventData={eventData}
+          getEventInvites={getEventInvites}
+          // userData={userData}
+          // getUserInvites={getUserInvites}
+          eventInviteData={eventInviteData}
+          userInviteData={userInviteData}
+          handleEditEvent={handleEditEvent}
+          handleDeleteEvent={handleDeleteEvent}
+          handleDeleteInvite={handleDeleteInvite}
+        />
       </div>
     </ChakraProvider>
   )
