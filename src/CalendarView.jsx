be7f0fab4b7a02a159c1f69f5dd700f9b4745084
Rebecca <rebeccaquey@gmail.com>
2024-02-26 18:02:57 -0800

    update calendarView to show the users event on the calendar, but cannot click on each event to view individual details

--- src/CalendarView.jsx
+++ src/CalendarView.jsx
@@ -1,33 +1,34 @@
 import React from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
-import listPlugin from '@fullcalendar/list';
-import timeGridPlugin from '@fullcalendar/timegrid';
 
+const CalendarView = (props) => {
+  const events = [];
+  //currently just views the calendar with the user's events, but cant click on each event to see more details
+  if (props.userInviteData) {
+    props.userInviteData.forEach(ele => {
+      const event = {
+        eventId: ele.id,
+        title: ele.event_name,
+        start: ele.event_start,
+        description: ele.event_description,
+        owner: ele.event_owner,
+        length: ele.event_length,
+        location: ele.event_location
+      }
+      events.push(event);
+    });
+  }
 
-
-const events = [
-  { title: 'Meeting', start: new Date() }
-]
-
-const CalendarView = (props) => (
+  return (
   <div>
     <FullCalendar 
       plugins={[dayGridPlugin]}
       initialView='dayGridMonth'
       weekends={true}
       events={events}
-      eventContent={renderEventContent}
     />
   </div>
-);
-
-function renderEventContent(eventInfo) {
-  return (
-    <>
-      <b>{eventInfo.timeText}</b>
-      <i>{eventInfo.event.title}</i>
-    </>
   )
 }
 
